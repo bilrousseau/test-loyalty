4d228eb1d99d4514f2e897733533dc68e164e52a
Bill Rousseau <bill.rousseau@doc.fr>
2017-09-11 02:11:06 +0200

    configure FOSRestBundle

--- ktb/app/AppKernel.php
+++ ktb/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new KTB\CrudBundle\KTBCrudBundle(),
+            new FOS\RestBundle\FOSRestBundle()
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
