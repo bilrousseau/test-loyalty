79e6491c32a6a06fd05543ee02890be242346936
Bill Rousseau <bill.rousseau@doc.fr>
2017-09-11 08:08:14 +0200

    First draft to show/create products

--- ktb/src/KTB/CrudBundle/Controller/ProductController.php
+++ ktb/src/KTB/CrudBundle/Controller/ProductController.php
@@ -2,12 +2,49 @@
 
 namespace KTB\CrudBundle\Controller;
 
+use KTB\CrudBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+# Use to convert data from request
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+# Use to fetch parameters if param_fetcher_listener is true
+# use FOS\RestBundle\Request\ParamFetcherInterface;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\Annotations\View;
 
 class ProductController extends Controller
 {
-    public function indexAction()
+	/**
+     * @Rest\Get(
+     *     path = "/products/{id}",
+     *     name = "product_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @View
+     */
+    public function showAction(Product $product)
     {
-        return $this->render('KTBCrudBundle:Default:index.html.twig');
+        return $product;
+    }
+
+    /**
+     * @Rest\Post(
+     *    path = "/products",
+     *    name = "product_create"
+     * )
+     * @Rest\View(StatusCode = 201)
+     * @ParamConverter("product", converter="fos_rest.request_body")
+     */
+    public function createAction(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+
+        return $this->view($product, Response::HTTP_CREATED, ['Location' => $this->generateUrl('product_show', ['id' => $product->getId(), UrlGeneratorInterface::ABSOLUTE_URL])]);
     }
 }
